00e1d4d0d9e22803f68db2402183ebace1c4c813
Huo Linhe <linhehuo@gmail.com>
2021-04-21 19:18:31 +0800

    feat: add chunk size option to reduce error

--- src/main.rs
+++ src/main.rs
@@ -27,11 +27,6 @@ pub mod utils;
 use taos::TaosError;
 use utils::*;
 //use protos::WriteRequest;
-
-type DatabaseExistsMap = Arc<RwLock<FnvHashMap<String, STableExistsMap>>>;
-type STableExistsMap = RwLock<FnvHashMap<String, TableExistsMap>>;
-type TableExistsMap = RwLock<FnvHashSet<String>>;
-
 fn table_name_escape(name: &str) -> String {
     let s = name
         .replace(":", "_")
@@ -68,7 +63,9 @@ async fn handle_stable_schema<'prom>(
         .describe(&format!("{}.{}", database, &stable_name))
         .await;
     trace!("schema: {:?}", &schema);
+    let _ = state.create_table_lock.lock().unwrap();
     if let Err(taos::Error::RawTaosError(taos::TaosError { code, ref err })) = schema {
+        let _ = state.create_table_lock.lock().unwrap();
         match code {
             taos::TaosCode::MnodeInvalidTableName => {
                 // create super table
@@ -79,7 +76,7 @@ async fn handle_stable_schema<'prom>(
                     34, // taghash length
                     labels
                         .iter()
-                        .map(|label| { format!("t_{} binary({})", label.name, 128) })  // TODO: default binary length is 128 
+                        .map(|label| { format!("t_{} binary({})", label.name.to_lowercase(), 128) })  // TODO: default binary length is 128 
                         .join(", ")
                 );
                 trace!("exec sql: {}", &sql);
@@ -91,7 +88,7 @@ async fn handle_stable_schema<'prom>(
             taos::TaosCode::MnodeDbNotSelected => {
                 // create database
                 let sql = format!("create database {}", database);
-                taos.exec(&sql).await?;
+                dbg!(taos.exec(&sql).await?);
                 // create super table
                 let sql = format!(
                     "create stable if not exists {}.{} (ts timestamp, value double) tags (taghash binary({}), {})",
@@ -100,7 +97,7 @@ async fn handle_stable_schema<'prom>(
                     34, // taghash length
                     labels
                         .iter()
-                        .map(|label| { format!("t_{} binary({})", label.name, 128) })  // TODO: default binary length is 128 
+                        .map(|label| { format!("t_{} binary({})", label.name.to_lowercase(), 128) })  // TODO: default binary length is 128 
                         .join(", ")
                 );
                 trace!("exec sql: {}", &sql);
@@ -128,7 +125,10 @@ async fn handle_stable_schema<'prom>(
         if !fields.contains(&format!("t_{}", label.name.to_lowercase())) {
             let sql = format!(
                 "alter stable {}.{} add tag t_{} binary({})",
-                database, stable_name, &label.name.to_lowercase(), 128
+                database,
+                stable_name,
+                &label.name.to_lowercase(),
+                128
             );
             trace!("add tag {} for stable {}: {}", label.name, stable_name, sql);
             taos.exec(&sql).await?;
@@ -142,13 +142,17 @@ async fn handle_stable_schema<'prom>(
     let taghash = md5sum(tagmap.values().join("").as_bytes());
 
     // create sub table;
+
     let sql = format!(
         "create table if not exists {}.{} using {}.{} (taghash,{}) tags(\"{}\",{})",
         database,
         table_name,
         database,
         stable_name,
-        tagmap.keys().map(|v| format!("t_{}", v.to_lowercase())).join(","),
+        tagmap
+            .keys()
+            .map(|v| format!("t_{}", v.to_lowercase()))
+            .join(","),
         taghash,
         tagmap
             .values()
@@ -170,7 +174,7 @@ async fn handle_table_schema(
         // handle stable
         handle_stable_schema(state, database, ts).await?;
     }
-    trace!("handle table schema");
+    debug!("handle table schema done");
     Ok(())
 }
 async fn write_tdengine_from_prometheus(
@@ -181,7 +185,7 @@ async fn write_tdengine_from_prometheus(
     debug!("Write tdengine from prometheus write request");
     let taos = state.pool.get()?;
     // build insert sql
-    let sql = req
+    let sql_iter = req
         .timeseries
         .iter()
         .map(|ts| {
@@ -195,49 +199,47 @@ async fn write_tdengine_from_prometheus(
             let tag_values = labels.iter().map(|label| &label.value).join("");
             let table_name = format!("{}{}", metrics_name, tag_values);
             let table_name = format!("{}.md5_{}", database, md5sum(table_name.as_bytes()));
-            ts.samples
-                .iter()
-                .map(|sample| {
-                    format!(
-                        "{} values ({}, {})",
-                        table_name, sample.timestamp, sample.value
-                    )
-                })
-                .join(" ")
+            ts.samples.iter().map(move |sample| {
+                format!(
+                    " {} values ({}, {})",
+                    table_name, sample.timestamp, sample.value
+                )
+            })
         })
-        .join(" ");
-    let sql = format!("insert into {}", sql);
+        .flatten()
+        .chunks(state.opts.chunk_size);
 
-    if let Err(taos_err) = taos.query(&sql).await {
-        if let taos::Error::RawTaosError(err) = &taos_err {
-            match err.code {
-                TaosCode::MnodeDbNotSelected | TaosCode::ClientDbNotSelected => {
-                    let _ = state.create_table_lock.lock().unwrap();
-                    taos.create_database(database).await?;
-                    handle_table_schema(&state, database, &req).await?;
-                    taos.query(&sql).await?;
-                    return Ok(());
+    for mut chunk in sql_iter.into_iter() {
+        let sql = chunk.join("");
+        let sql = format!("insert into {}", sql);
+        debug!("chunk sql length is {}", sql.len());
+
+        if let Err(err) = taos.query(&sql).await {
+            if let taos::Error::RawTaosError(err) = &err {
+                match err.code {
+                    TaosCode::MnodeDbNotSelected
+                    | TaosCode::ClientDbNotSelected
+                    | TaosCode::ClientInvalidTableName
+                    | TaosCode::MnodeInvalidTableName => {
+                        handle_table_schema(&state, database, &req).await?;
+                        taos.query(&sql).await?;
+                        return Ok(());
+                    }
+                    code => {
+                        warn!("insert into tdengine error: [{}]{}", code, err);
+                    }
                 }
-                TaosCode::ClientInvalidTableName | TaosCode::MnodeInvalidTableName => {
-                    let _ = state.create_table_lock.lock().unwrap();
-                    handle_table_schema(&state, database, &req).await?;
-                    taos.query(&sql).await?;
-                    return Ok(());
-                }
-                code => {
-                    warn!("insert into tdengine error: [{}]{}", code, err);
-                }
+            } else {
+                error!("error with query [{}] : {}", sql.len(), err);
             }
+            Err(err)?
         }
-        Err(taos_err)?
     }
+
     Ok(())
 }
 #[post("/adapters/prometheus/{database}")]
 async fn prometheus(
-    //pool: web::Data<TaosPool>,
-    //mgr: web::Data<DatabaseExistsMap>,
-    //create_table_lock: web::Data<Mutex<i32>>,
     state: web::Data<AppState>,
     database: web::Path<String>,
     bytes: Bytes,
@@ -269,7 +271,7 @@ async fn prometheus(
 }
 
 /// TDengine adapter for prometheus.
-#[derive(Debug, Clap)]
+#[derive(Debug, Clone, Clap)]
 #[clap(setting = clap::AppSettings::ColoredHelp)]
 #[clap(version, author)]
 struct Opts {
@@ -292,9 +294,13 @@ struct Opts {
     listen: String,
     #[clap(short, long, default_value = "10")]
     workers: usize,
+    #[clap(short, long, default_value = "200")]
+    chunk_size: usize,
 }
 
+#[derive(Debug)]
 pub struct AppState {
+    opts: Opts,
     pool: taos::TaosPool,
     create_table_lock: Mutex<i32>,
 }
@@ -326,7 +332,10 @@ async fn main() -> Result<()> {
     let taos_pool = r2d2::Pool::builder()
         .max_size(opts.workers as u32 * 2)
         .build(taos_cfg)?;
+    let workers = opts.workers;
+    let listen = opts.listen.clone();
     let state = web::Data::new(AppState {
+        opts,
         pool: taos_pool.clone(),
         create_table_lock: Default::default(),
     });
@@ -336,8 +345,8 @@ async fn main() -> Result<()> {
             .wrap(Logger::default())
             .service(prometheus)
     })
-    .workers(opts.workers)
-    .bind(&opts.listen)?
+    .workers(workers)
+    .bind(&listen)?
     .run();
 
     server.await?;
