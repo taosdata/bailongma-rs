047396f1c9e8a77a02e4e9994756624b4f46e852
Huo Linhe <linhehuo@gmail.com>
2021-06-08 11:31:54 +0800

    utils(prom-decode): support read/write request

--- src/bin/prom-decode.rs
+++ src/bin/prom-decode.rs
@@ -1,10 +1,28 @@
 use std::path::PathBuf;
+use std::str::FromStr;
 
 use anyhow::Result;
 use clap::Clap;
 use prost::Message;
 
 use bailongma::*;
+
+#[derive(Debug, Clone)]
+enum PromType {
+    ReadRequest,
+    WriteRequest,
+}
+impl FromStr for PromType {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "read" | "read-request" | "readrequest" | "readreq" => Ok(PromType::ReadRequest),
+            "write" | "write-request" | "writerequest" | "writereq" => Ok(PromType::ReadRequest),
+            _ => Err("no match"),
+        }
+    }
+}
 
 /// TDengine adapter for prometheus.
 #[derive(Debug, Clone, Clap)]
@@ -18,12 +36,41 @@ struct Opts {
     /// Pretty print
     #[clap(short, long)]
     pretty: bool,
+    /// Read or Write
+    #[clap(short = 't', long, default_value = "read")]
+    prom_type: PromType,
+}
+fn write_json<T: ?Sized>(opts: &Opts, data: &T) -> Result<()> 
+where
+    T: serde::Serialize, 
+{
+    match &opts.json {
+        None => {
+            let output = std::io::stdout();
+            if opts.pretty {
+                serde_json::to_writer_pretty(output, &data)?;
+            } else {
+                serde_json::to_writer(output, &data)?;
+            }
+        }
+        Some(path) => {
+            let output = std::fs::File::create(path)?;
+
+            if opts.pretty {
+                serde_json::to_writer_pretty(output, &data)?;
+            } else {
+                serde_json::to_writer(output, &data)?;
+            }
+        }
+    }
+    Ok(())
+
 }
 
 fn main() -> Result<()> {
     let opts = Opts::parse();
     env_logger::init();
-    let bytes = std::fs::read(opts.path)?;
+    let bytes = std::fs::read(&opts.path)?;
     let mut decoder = snap::raw::Decoder::new();
     let decompressed = decoder
         .decompress_vec(&bytes)
@@ -34,25 +81,14 @@ fn main() -> Result<()> {
         decompressed.len()
     );
 
-    let write_request = WriteRequest::decode(&mut decompressed.as_ref()).expect("deserialzied ok");
-
-    match opts.json {
-        None => {
-            let output = std::io::stdout();
-            if opts.pretty {
-                serde_json::to_writer_pretty(output, &write_request)?;
-            } else {
-                serde_json::to_writer(output, &write_request)?;
-            }
+    match opts.prom_type {
+        PromType::ReadRequest => {
+            let request = ReadRequest::decode(&mut decompressed.as_ref()).expect("deserialzied ok");
+            write_json(&opts, &request)?;
         }
-        Some(path) => {
-            let output = std::fs::File::create(path)?;
-
-            if opts.pretty {
-                serde_json::to_writer_pretty(output, &write_request)?;
-            } else {
-                serde_json::to_writer(output, &write_request)?;
-            }
+        PromType::WriteRequest => {
+            let request = WriteRequest::decode(&mut decompressed.as_ref()).expect("deserialzied ok");
+            write_json(&opts, &request)?;
         }
     }
 
